0b6723ed62432c19b7a16128dcf18d125e90309b
Paul Seyfert <pseyfert@physi.uni-heidelberg.de>
2013-12-26 23:52:56 +0100

    bare files for derived ZooReader, test pre-commit hook

---
+++ Looper/ZooOneBranchReader.cpp
@@ -0,0 +1,18 @@
+#include "ZooOneBranchReader.h"
+
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : ZooOneBranchReader
+//
+// 2013-12-26 : Paul Seyfert <pseyfert@cern.ch>
+//-----------------------------------------------------------------------------
+
+
+
+ZooOneBranchReader::ZooOneBranchReader()
+{
+}
+
+ZooOneBranchReader::~ZooOneBranchReader() {;}
+
+
---
+++ Looper/ZooOneBranchReader.h
@@ -0,0 +1,28 @@
+#ifndef ZOOONEBRANCHREADER_H
+#define ZOOONEBRANCHREADER_H 1
+
+
+/** @class ZooOneBranchReader ZooOneBranchReader.h
+ *  
+ *
+ * @author Paul Seyfert <pseyfert@cern.ch>
+ * @date   2013-12-26
+ */
+class ZooOneBranchReader : public ZooReader {
+
+public:
+
+  /// Standard Constructor
+  ZooOneBranchReader();
+
+  virtual ~ZooOneBranchReader(); ///< Destructor
+
+protected:
+
+
+private:
+
+
+};
+#endif // INCLUDE_ZOOONEBRANCHREADER_H
+
